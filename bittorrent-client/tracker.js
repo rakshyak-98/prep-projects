ceae825f718ee43d8812fad85660170096894c69
rakshyak-98 <rakshak1998@gmail.com>
2024-05-31 17:42:20 +0530

    add announce message request and response

--- bittorrent-client/tracker.js
+++ bittorrent-client/tracker.js
@@ -1,6 +1,8 @@
 const crypto = require("node:crypto");
 const dgram = require("node:dgram");
 
+const util = require("./util.cjs");
+
 const urlParse = require("node:url").parse;
 const Buffer = require("node:buffer").Buffer;
 
@@ -38,7 +40,36 @@ function buildConnReq() {
 	return buf;
 }
 
-function buildAnnounceReq(connId) {}
+function buildAnnounceReq(connId, torrent, port=6881) {
+	const buf = Buffer.allocUnsafe(98);
+	// connection id
+	connId.copy(buf, 0);
+	// action
+	buf.writeUInt32BE(1, 0);
+	// transaction id
+	crypto.randomBytes(4).copy(buf, 12);
+	// info hash
+	// torrentParser.infoHash(torrent).cop(buf, 16);
+	// peerId
+	util.genId().copy(buf, 36);
+	// downloaded
+	Buffer.alloc(8).copy(buf, 56);
+	// left
+	// torrentParser.size(torrent).copy(buf, 64);
+	// uploaded
+	Buffer.alloc(8).copy(buf, 72);
+	// event
+	buf.writeUInt32BE(0, 80);
+	// ip address
+	buf.writeUInt32BE(0, 80);
+	// key
+	crypto.randomBytes(4).copy(buf, 88);
+	// num want
+	buf.writeInt32BE(-1, 92);
+	// port
+	buf.writeUInt16BE(port, 96);
+	return buf;
+}
 
 function parseConnResp(resp) {
 	return {
@@ -48,5 +79,24 @@ function parseConnResp(resp) {
 	};
 }
 
-function parseAnnounceResp(resp) {}
-
+function parseAnnounceResp(resp) {
+	function group(iterable, groupSize){
+		let groups = new Array();
+		for(let i = 0; i < iterable.length; i+=groupSize){
+			groups.push(iterable.slice(i, i + groupSize));
+		}
+		return groups;
+	}
+	return {
+		action: resp.readUInt32BE(0),
+		transactionId: resp.readUInt32BE(4),
+		leechers: resp.readUInt32BE(8),
+		seeders: resp.readUInt32BE(12),
+		peers: group(resp.slice(20), 6).map(address => {
+			return {
+				ip: address.slice(0, 4).join('.'),
+				port: address.readUInt16BE(4),
+			}
+		})
+	}
+}
