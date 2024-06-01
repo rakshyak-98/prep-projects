274c576e67a7a61cdef0bca1970de83c13d7f9b0
rakshyak-98 <rakshak1998@gmail.com>
2024-06-01 06:24:42 +0530

    add non-handshake message handlers

--- bittorrent-client/src/download.js
+++ bittorrent-client/src/download.js
@@ -10,12 +10,43 @@ module.exports = (torrent) => {
 
 function download(peer) {
 	const socket = new net.Socket();
-	socket.on("error", console.log);
-	socket.connect(port, ip, function () {
+	socket.connect(peer.port, peer.id, () => {
 		socket.write(Buffer.from("hello world"));
 	});
-	socket.on("data", (responseBuffer) => {
-		console.log(responseBuffer);
+	onWholeMsg(socket, data => {
+
+	})
+}
+
+function onWholeMsg(socket, callback) {
+	let saveBuf = Buffer.alloc(0);
+	let handshake = true;
+	socket.on("data", (recvBuf) => {
+		const msgLen = () => (handshake ? saveBuf.readUInt8(0) + 49 : saveBuf.readInt32BE(0) + 4);
+		saveBuf = Buffer.concat([saveBuf, recvBuf]);
+		while (saveBuf.length >= 4 && saveBuf.length >= msgLen()) {
+			callback(saveBuf.slice(0, msgLen()));
+			saveBuf = saveBuf.slice(msgLen());
+			handshake = false;
+		}
 	});
 }
 
+function msgHandler(msg, socket) {
+	if (inHandshake(msg)){
+		socket.write(message.buildInterested());
+	} else {
+		const m = message.parse(msg);
+		if(m.id === 0) chokeHandler();
+		if(m.id === 1) unChokeHandler();
+		if(m.id === 4) haveHandler(m.payload);
+		if(m.id === 5) bitfieldHandler(m.payload);
+		if(m.id === 7) pieceHandler(m.payload);
+	}
+}
+
+function chokeHandler(){}
+function unChokeHandler(){}
+function haveHandler(){}
+function bitfieldHandler(){}
+function pieceHandler(){}
